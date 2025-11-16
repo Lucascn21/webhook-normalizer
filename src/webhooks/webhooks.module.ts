dc8aaf19c35be3cf19a3dac5b416d27fbb8b9e1b
lucascn21 <lucascn21@gmail.com>
2025-11-16 11:04:59 -0300

    refactor: add DuplicateRequestGuard to WebhooksModule providers

--- src/webhooks/webhooks.module.ts
+++ src/webhooks/webhooks.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WebhooksService } from './webhooks.service';
 import { WebhooksController } from './webhooks.controller';
+import { DuplicateRequestGuard } from './guards/duplicate-request.guard';
 
 @Module({
   controllers: [WebhooksController],
-  providers: [WebhooksService],
+  providers: [WebhooksService, DuplicateRequestGuard],
 })
 export class WebhooksModule {}
